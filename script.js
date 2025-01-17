de66b19966d9bd37b839b2f9ee05fc985e197db7
rschwab58 <rschwab27@students.stab.org>
2025-01-17 11:06:31 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ var cholestral = getColumn(url, 8);
 var saturatedFats= getColumn(url, 9);
 var sodium = getColumn(url, 10);
 
+// this function creates a list of meal names under a specific type of food 
+// the specificMealName list loops through the typesOfFood and if the tpye of food matches
+// then it gets added to the list
+//if there is zero items in the list then it returns "no meals found"
 function specificMealName(typeOfFood){
     var specificMealName =[]
     for(var i=0; i < typeOfFoods.length; i++) {
@@ -19,12 +23,16 @@ function specificMealName(typeOfFood){
         }
     }
     if(specificMealName.length == 0){
-        specificMealName.push("meal item not found")
+        specificMealName.push("no meals found")
     }
     return specificMealName;
 }
-console.log(specificMealName("pizza"))
+//console.log(specificMealName("pizza"))
 
+// this function creates a list of resturants that sell whatever type of food is inputed
+//this loop goes through and checks if the type of food matches then after adds it
+//the name of the resturant to the list without duplicates.
+//if there is zero items in the list then it returns "no items avalible"
 function specificResturantName(typeOfFoodAtResturant){
     var specificResturantName =[]
     for(var i=0; i < typeOfFoods.length; i++) {
@@ -37,10 +45,13 @@ function specificResturantName(typeOfFoodAtResturant){
     }
     return specificResturantName;
 }
-console.log(specificResturantName("pizza"))
+//console.log(specificResturantName("pizza"))
 
-
-
+// this function creates a list of meal from a spesific resturant 
+// it will list all the meals from that resturant also with their protien count
+// if the name in not fully complete then it will still register as in 
+// "wendy" for "wendys" so you can partley include the name 
+//if there is zero items in the list then it returns "resturant not found"
 function amountOfProteinInAMeal(nameOfResturant){
     // if(typeOf(nameOfResturant) != string){
     //     return ["enter a string"];
@@ -48,7 +59,7 @@ function amountOfProteinInAMeal(nameOfResturant){
     var proteinInAMeal =[]
     for(var i=0; i < resturant.length; i++){
         if(resturant[i].toLowerCase().includes(nameOfResturant.toLowerCase() )){
-            proteinInAMeal.push(mealName[i] + ": " + protein[i]);
+            proteinInAMeal.push(mealName[i] + ": with " + protein[i]+ " grams of protein");
             }
         }
         if (proteinInAMeal.length == 0){
@@ -56,8 +67,15 @@ function amountOfProteinInAMeal(nameOfResturant){
         }
         return proteinInAMeal;
     }
-console.log(amountOfProteinInAMeal("wendy"))
+//console.log(amountOfProteinInAMeal("wendy"))
 
+// this function creates a avergae calorie count of all the items from that resturant
+//it adds up total number of calories and adds 1 to the numingroup if it is
+//from that resturant, after it gets the total it divides the total by the 
+//number of numbers in the numInGroup
+// if the name in not fully complete then it will still register as in 
+// "wendy" for "wendys" so you can partley include the name 
+//if there is zero items in the list then it returns "resturant not found"
 function averageAmountOfCaloriesPerResturant(nameOfResturant){
     var toltalCaloriesInAMeal = 0; 
     var numInGroup = 0;
@@ -72,8 +90,14 @@ function averageAmountOfCaloriesPerResturant(nameOfResturant){
     }
         return toltalCaloriesInAMeal/numInGroup;
 }
-console.log(averageAmountOfCaloriesPerResturant("wendy"))
+//console.log(averageAmountOfCaloriesPerResturant("wendy"))
 
+// this function is trying to find the lowest calorie item and name under a spesific type of food
+// it goes through the type of food list  then sees if the calories count is less and if it is
+//it makes that the new lowest calorie item and contiues to do that until it finds the lowest 
+//calorie item in that type of food
+// it will return both the name of meal and the amount of calories
+//if there is zero items in the list then it returns "type not found"
 function minAmountOfCaloriesInAMeal(foodType){
     var minCaloriesInMeal = 500000000;
     var lowestCalItem = ""
@@ -91,6 +115,7 @@ function minAmountOfCaloriesInAMeal(foodType){
        
     }
 console.log(minAmountOfCaloriesInAMeal("pizza"))
+
 
 function getColumn(url, columnNumber){
     var column = [];
